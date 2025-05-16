fb54f91612202a63342feafabdc3cb5302079592
JorgeToledoIporre <jorg.toledo@duocuc.cl>
2025-05-16 11:24:55 -0400

    cambio de nombre de api

--- src/main/java/com/EcoMarket/producto/controller/InventarioController.java
+++ src/main/java/com/EcoMarket/producto/controller/InventarioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/inventario")
+@RequestMapping("/api/v1/inventario")
 public class InventarioController {
 
     @Autowired
