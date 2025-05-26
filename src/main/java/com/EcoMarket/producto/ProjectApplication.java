8dfbfe7902e6cac4de6501846a038be0de69e3ac
JorgeToledoIporre <jorg.toledo@duocuc.cl>
2025-05-26 14:39:25 -0400

    se hacen secretas las claves

--- src/main/java/com/EcoMarket/producto/ProjectApplication.java
+++ src/main/java/com/EcoMarket/producto/ProjectApplication.java
@@ -3,10 +3,13 @@ package com.EcoMarket.producto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
+		Dotenv.load();
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
