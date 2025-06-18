3b574c11e57cd4dad53d52fae772a88f57e1760e
JorgeToledoIporre <jorg.toledo@duocuc.cl>
2025-06-18 18:58:58 -0400

    Update ProjectApplicationTests.java

--- src/test/java/com/EcoMarket/Project/ProjectApplicationTests.java
+++ src/test/java/com/EcoMarket/Project/ProjectApplicationTests.java
@@ -3,9 +3,10 @@ package com.EcoMarket.Project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProjectApplicationTests {
+import com.EcoMarket.producto.ProjectApplication;
 
+@SpringBootTest(classes = ProjectApplication.class)
+class ProjectApplicationTests {
 	@Test
 	void contextLoads() {
 	}
